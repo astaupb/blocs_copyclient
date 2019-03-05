444ca7506b48a75836ab834c56f95e1777a4d071
Leon Tappe <ltappe@mail.uni-paderborn.de>
2019-03-05 05:23:26 +0100

    extend user model

--- lib/src/models/user.dart
+++ lib/src/models/user.dart
@@ -1,16 +1,28 @@
 class User {
   String name;
   int userId;
+  int card;
+  int pin;
   int activeTokens;
   String _token;
   int tokenId;
   int credit;
 
-  User({this.name, this.userId, this.activeTokens, this.tokenId, this.credit});
+  User({
+    this.name,
+    this.userId,
+    this.card,
+    this.pin,
+    this.activeTokens,
+    this.tokenId,
+    this.credit,
+  });
 
   factory User.fromMap(dynamic obj) => User(
         name: obj['name'],
         userId: obj['id'],
+        card: obj['card'],
+        pin: obj['pin'],
         credit: obj['credit'],
         activeTokens: obj['tokens'],
         tokenId: obj['token_id'],
@@ -23,6 +35,8 @@ class User {
   Map<String, dynamic> toMap() => {
         'name': name,
         'id': userId,
+        'card': card,
+        'pin': pin,
         'credit': credit,
         'tokens': activeTokens,
         'tokenId': tokenId,
